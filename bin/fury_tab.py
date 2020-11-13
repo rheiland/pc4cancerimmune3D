6703d7c39666a57ec2a8126a0a5cd50ced763b3c
Javier Guaje <jrguajeg@iu.edu>
2020-11-12 19:39:27 -0500

    Minor changes on fury_tab.

--- bin/fury_tab.py
+++ bin/fury_tab.py
@@ -1,10 +1,24 @@
 from ipywidgets import Output
-from IPython.display import display, HTML
+from IPython.display import HTML, Javascript, display
+
 
 class FuryTab(object):
 
     def __init__(self):
-        # self.tab = Output(layout={'height': '600px'})
-        self.tab = Output(layout={'height': 'auto'})
-        self.tab.append_display_data(HTML(filename='doc/fury_client.html'))
-        
+        self.tab = Output(layout={'height': '600px'})
+        #self.tab = Output(layout={'height': 'auto'})
+        #self.tab.append_display_data(HTML(filename='doc/fury_client.html'))
+        #self.tab.append_display_data(Javascript(
+        #    filename='doc/FuryWebClient.js'))
+        js = \
+            """
+            element.css({backgroundColor: "gray", margin: "0px", 
+                         padding: "0px"});
+            element.height(100);
+            element.width(100);
+            element.html( $( "<div id='fury'></div>" ) );
+            
+            const divRenderer = document.getElementById('fury');
+            """
+        self.tab.append_display_data(Javascript(js))
+        #self.tab.append_display_data(Javascript(filename='doc/FuryWebClient.js'))
